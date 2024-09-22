624b73353ed203383f650b95c79ed2036dd6d26f
Sakib Fakir <fakirsakib22232>
2024-09-23 01:05:19 +0600

    transcition history add

--- Custome.js
+++ Custome.js
@@ -5,10 +5,52 @@
 
 
 
-function handelAllinputValue(id){
-    
+function handelAllinputValue(id) {
+
     let inputvalue = document.getElementById(id).value;
     return inputvalue;
 }
 
 //catch input value 
+
+let history = document.getElementById("history");
+let ans = [];
+function showtranscitionHistory(ammount) {
+    ans.push(ammount);
+
+
+    history.innerHTML = ' ';
+
+
+
+    ans.forEach((item, key) => {
+        let newline = document.createElement("p");
+        
+        
+
+        newline.innerText = ` your histroy  ${item} `;
+        newline.style.fontSize="25px";
+        newline.style.marginTop="10px";
+        newline.style.height="20px";
+        
+        newline.style.padding="4px";
+
+        
+    
+        history.appendChild(newline);
+
+
+
+
+    })
+    
+    
+
+
+
+
+
+
+
+
+}
--- sakib.js
+++ sakib.js
@@ -26,23 +26,46 @@ let Donation_button_main = document.getElementById("Donation-button-main");
 
 
 Donation_button_main.addEventListener("click", ()=>{
+
     
     Donation_button_main.classList.add("color");
     History_button_main.classList.remove("color");
+
+    document.getElementById("header").classList.remove('hidden');
+    document.getElementById("main").classList.remove("hidden");
+    document.getElementById("footer").classList.remove("hidden")
+    
+    document.getElementById("history").classList.remove("show2");
+
+  
+
     
     
-
-
-
-
 
 })
+
+// transction hsitory
+
 
 History_button_main.addEventListener("click", ()=>{
     
     History_button_main.classList.add("color");
-    Donation_button_main.classList.remove("color")
+    Donation_button_main.classList.remove("color");
 
+    ///header main footer
+
+    document.getElementById("header").classList.add('hidden');
+    document.getElementById("main").classList.add("hidden");
+    document.getElementById("footer").classList.add("hidden");
+
+    // document.getElementById("donation_button_main").classList.add('show');
+
+    document.getElementById("history").classList.add("show2");
+    
+
+
+
+    
 
 
 })
@@ -61,8 +84,13 @@ button.addEventListener("click",()=>{
  
     const ans = handelAllinputValue('input');
 
+    // let test = showtranscitionHistory('input');
+
+    showtranscitionHistory(input.value);
+
     taka=parseInt(taka)-parseInt(ans);
     ammount_header.innerText=taka;
+
 
     console.log("button 1")
 })
@@ -76,14 +104,17 @@ button2.addEventListener("click",()=>{
 
     taka=parseInt(taka)-parseInt(ans);
     ammount_header.innerText=taka;
+    showtranscitionHistory(input2.value)
 
     console.log("button 1")
 })
+
 let input3 = document.getElementById("input3");
 let button3 = document.getElementById("button3");
 button3.addEventListener("click",()=>{
  
     const ans = handelAllinputValue('input3');
+    showtranscitionHistory(input3.value)
 
     taka=parseInt(taka)-parseInt(ans);
     ammount_header.innerText=taka;
